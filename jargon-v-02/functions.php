80acebbed4e693c22b1af1f1e5639e709c88e46c
Dana <dana@algot.ca>
2019-10-03 09:01:28 -0600

    Added functions.php and edited styles.css

---
+++ jargon-v-02/functions.php
@@ -0,0 +1,30 @@
+<!-- Directions to wordpress -->
+
+
+
+<?php
+
+        if (  ! function_exists('setup_jargon')  ) {
+            //  add theme support form wordpress
+
+            add_theme_support('title-tag');
+
+        }
+
+        add_theme_support('after_setup_theme',      'setup_jargon');
+
+
+        function jargon_setup(){
+            wp_enqueue_style(   ' jargon-styles ',      get_stylesheet_uri()   ); //linking to the style sheet
+        }
+
+        add_action(   'wp_enqueue_scripts',     'jargon-styles'  );
+
+        //echo get_stylesheet_uri(); //gets the url to the style sheet
+
+        //echo get_template_directory_uri(); //url, including the domain name
+
+       // echo get_template_directory(); //computer path, this is how you would start pathing to files
+
+
+?>
