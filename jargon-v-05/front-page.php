70a477b4dc0c21fb46d00c5bf3c93c5683759e0c
Dana <dana@algot.ca>
2019-10-16 11:09:02 -0600

    Creating custom posts

--- jargon-v-05/front-page.php
+++ jargon-v-05/front-page.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 <main>
   The Front Page Template
+
+  <a href="https://dalgot.ca/dmit2032/labthree/movies/five-feet-apart/">Five Feet Apart</a>
 </main>
 <?php get_footer(); ?>
--- jargon-v-05/functions.php
+++ jargon-v-05/functions.php
@@ -10,7 +10,8 @@
    }
 
    add_theme_support("after_setup", "jargon_setup");
- 
+//    This disables the gutenburg editor - Some themes might not be compatible
+   add_filter('use_block_editor_for_post', '__return_false', 10);
 
 
 
@@ -26,7 +27,31 @@
     add_action('wp_enqueue_scripts', 'jargon_styles');
 
 
+//function creates a custom post type of movies
+function create_post_type_movies()
+{
+    // creates label names for the post type in the dashboard the post panel and in the toolbar.
+    $labels = array( //Lables are all the places the movie would be reverenced 
+               'name' => __('Movies'),
+               'singular_name' => __('Movie'),
+               'add_new' => 'Add New Movie',
+               'edit_item' => 'Create Movie'
+    );
+    // creates the post functionality that you want for a full listing see the link attached above
+    $args = array(
+        'labels' => $labels, //Lables are all the places the movie would be reverenced 
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'movies'),
+         'menu_position' => 5, //position where this will show on the dashboard menu
+         'menu_icon' => 'dashicons-video-alt2', //Icon that shows on the dashboard
+         'supports'=> array('title', 'editor', 'custom-fields') // things we give to users to create the post
+    );
 
+   register_post_type('movies', $args  ); //this is the post type for the file - ours is single-movies.php - Creates custom posts
+}
+// Hooking up our function to theme setup
+add_action('init', 'create_post_type_movies');
  
 
 ?>
